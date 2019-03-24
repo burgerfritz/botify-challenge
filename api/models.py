5cbc8cbcdcaee3bdc6e928edc3c2611e58c16b61
burgerfritz <frederic.sautot@gmail.com>
2019-03-24 11:31:35 +0100

    Created Town model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Town(models.Model):
+    region_code = models.PositiveSmallIntegerField()
+    region_name = models.CharField(max_length=50)
+    dept_code = models.CharField(max_length=3)
+    distr_code = models.PositiveSmallIntegerField()
+    code = models.IntegerField()
+    name = models.CharField(max_length=50)
+    population = models.PositiveIntegerField()
+    average_age = models.FloatField()
