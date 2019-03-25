2acac7293e029ed8b96df1c677e894e50bc7417c
burgerfritz <frederic.sautot@gmail.com>
2019-03-25 19:04:39 +0100

    Code refactor

--- project/urls.py
+++ project/urls.py
@@ -1,19 +1,4 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from api import views
+from api import viewsets
 
 from django.contrib import admin
 from django.urls import path, include
@@ -22,12 +7,11 @@ from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'towns', views.TownViewSet)
+router.register(r'users', viewsets.UserViewSet)
+router.register(r'towns', viewsets.TownViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('upload/<str:filename>', FileUploadView.as_view()),
 ]
